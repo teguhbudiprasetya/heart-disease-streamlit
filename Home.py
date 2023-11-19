76d1c37b5eb8bad3e1f574ae35ba0340591fc5eb
Teguh prasetya <a.tguhbdistyaprasetya@gmail.com>
2023-11-19 11:18:46 +0700

    initial commit

---
+++ Home.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+from streamlit_extras.app_logo import add_logo
+
+add_logo("https://www.google.com/url?sa=i&url=https%3A%2F%2Fkaciicons.tumblr.com%2Fpost%2F712348758156967936%2Fanya-forger-icons&psig=AOvVaw3o0EjeeRR8GmXa74iXi6Kx&ust=1696884152498000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCNCTspuo54EDFQAAAAAdAAAAABAX")
+st.markdown("# Home")
+st.text("""
+         Nama   : A. Teguh Budi Setya Prasetya
+         NIM    : 200411100144
+         Kelas  : Proyek Sains Data B
+         """)
+st.write("""
+         Email  : a.tguhbdistyaprasetya@gmail.com\n
+         """)
+
+st.markdown("# Ekstraksi Ciri dan klasifikasi Audio")
+st.text("Dataset berupa audio yang diekstrak menjadi 21 fitur numerik dan 7 class")
+
+
+
+
+
---
+++ README.md
@@ -0,0 +1,16 @@
+# audio-clasification-streamlit
+
+## Installation
+
+To install the required dependencies for this app, you can use the following command:
+
+```bash
+pip install -r requirements.txt
+```
+## Running the App
+To run the app, use the following command:
+
+```bash
+streamlit run home.py
+```
+This is the code for preprocessing and modeling, all of which is saved in Google Colab: [Google Colab Link](https://colab.research.google.com/drive/1XJS_HR2tNaV9Vz3yKqDTCHo8-OEsqgJ6?usp=sharing)
